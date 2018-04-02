1d2d81a0bc8194e99ac1f75ac0b7877369147635
Christiaan Lombard <base1.christiaan@gmail.com>
2018-04-02 20:37:05 +0200

    added flash messages

--- backend/pysql/restaurantmenu/flask-webserver.py
+++ backend/pysql/restaurantmenu/flask-webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from restaurantmenu import RestaurantCRUD, MenuItemCRUD
 from html import \
         HTML_LAYOUT, \
@@ -26,7 +26,6 @@ def restaurantDetail(r_id):
     return render_template('restaurant_detail.html', rest=rest, items=menuItems)
 
 
-
 @app.route('/restaurants/<int:r_id>/menu-items/new', methods=['GET', 'POST'])
 def createMenuItem(r_id):
     rest = restCrud.find(r_id)
@@ -37,9 +36,11 @@ def createMenuItem(r_id):
         item.price = request.form['price']
         item.description = request.form['description']
         menuCrud.create(item)
+        flash("Menu Item '%s' created" % item.name)
         return redirect(url_for('restaurantDetail', r_id=r_id))
 
     return render_template('menuitem_form.html', rest=rest, item=item, action='create')
+
 
 @app.route('/restaurants/<int:r_id>/menu-items/<int:m_id>/edit', methods=['GET', 'POST'])
 def editMenuItem(r_id, m_id):
@@ -51,9 +52,11 @@ def editMenuItem(r_id, m_id):
         item.price = request.form['price']
         item.description = request.form['description']
         menuCrud.update(item)
+        flash("Menu Item '%s' updated" % item.name)
         return redirect(url_for('restaurantDetail', r_id=r_id))
 
     return render_template('menuitem_form.html', rest=rest, item=item, action='update')
+
 
 @app.route('/restaurants/<int:r_id>/menu-items/<int:m_id>/delete', methods=['GET', 'POST'])
 def deleteMenuItem(r_id, m_id):
@@ -62,11 +65,13 @@ def deleteMenuItem(r_id, m_id):
 
     if request.method == 'POST':
         menuCrud.delete(item)
+        flash("Menu Item '%s' deleted" % item.name)
         return redirect(url_for('restaurantDetail', r_id=r_id))
 
     return render_template('menuitem_delete.html', rest=rest, item=item)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
