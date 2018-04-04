a5ad3a4eadf451ef5ac7141ab47f010bf5a9bea2
Christiaan Lombard <base1.christiaan@gmail.com>
2018-04-04 15:25:03 +0200

    separated restaurantmenu apps to basic and flask

---
+++ backend/apps/restaurantmenu-flask/database_setup.py
@@ -0,0 +1,50 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+### class declarations ###
+
+class Restaurant(Base):
+  __tablename__ = 'restaurant'
+  id = Column(Integer, primary_key = True)
+  name = Column(String(80), nullable = False)
+
+class MenuItem(Base):
+  __tablename__ = 'menu_items'
+  id = Column(Integer, primary_key = True)
+  name = Column(String(80), nullable = False)
+  course = Column(String(250))
+  description = Column(String(250))
+  price = Column(String(8))
+  restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+  restaurant = relationship(Restaurant)
+
+  @property
+  def serialize(self):
+    return {
+      'name': self.name,
+      'description': self.description,
+      'id': self.id,
+      'price': self.price,
+      'course': self.course,
+    }
+
+
+
+### configuration create db ###
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
+
+def get_session():
+    # Create session and connect to DB
+    engine = create_engine('sqlite:///restaurantmenu.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    return DBSession()
